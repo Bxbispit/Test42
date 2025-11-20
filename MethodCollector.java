8ba49dbf66a9a2c7d720a685ffcc2eb83a895075
Johannes Lüken <johannes.lueken@amcon.de>
2025-11-20 14:25:23 +0100

    add subtracNumber

--- MethodCollector.java
+++ MethodCollector.java
@@ -9,17 +9,22 @@ public class MethodCollector {
      * @param args
      */
     public static void main(String[] args) {
-        int methodCounter = 1;
+        int methodCounter = 2;
         // Erste Methode
         System.out.println("Method " + methodCounter + ":");
         aFirstMethod();
         methodCounter++;
         // hier kann die nächste Methode folgen.
+        System.out.println(subtracNumber(5,2));
     }
 
 
     private static void aFirstMethod() {
         System.out.println("MethodCollector aFirstMethod: This method has been created     by ");
+    }
+
+    private static int subtracNumber(int firstNumber, int secondNumber) {
+        return firstNumber-secondNumber;
     }
 
 }
